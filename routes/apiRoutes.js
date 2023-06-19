665e236659c0486230b3d5ef124d34b90e54063b
Ini. Duff CIDD / <calebdduff@gmail.com>
2023-06-19 16:40:58 +0100

    add routes for http API methods

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Note = require("../models/noteSchema")
+const mongoose = require("mongoose");
 
 // let notes = [
 //     {
@@ -24,17 +25,32 @@ const Note = require("../models/noteSchema")
 
 //GET request for a single resource
 // GET api/v1/notes/3
-router.get("/:id", (req, res) => {
+router.get("/:id", async(req, res) => {
     // res.send("Returns a single resource")
     const idx = req.params.id;
-    const singleNote = notes.find(note => note.id === Number(idx) );
-    res.json(singleNote)
+
+      //Check if the id is a valid MongoDB id
+      if(!mongoose.Types.ObjectId.isValid(idx)) {
+        return res.status(404).json({error: "No such note"})
+    }
+      // find note post by id
+      const note = await Note.findById(idx);
+
+      // if the note we are searching for does not exist
+      if(!note) {
+          return res.status(404).json({error: "No such note found!"})
+      }
+      res.status(200).json(note)
+    // const singleNote = notes.find(note => note.id === Number(idx) );
+    // res.json(singleNote)
 })
 
 // GET request to fetch all the resources in the collection
 // GET api/v1/notes
-router.get("/", (req, res) => {
-    res.json("ALL NOTES")
+router.get("/", async (req, res) => {
+    // res.json("ALL NOTES")
+    const notes = await Note.find({}).sort({createdAt: -1})
+    res.status(200).json(notes);
 })
 
 // POST request to create a new resource based on request data
@@ -56,14 +72,41 @@ router.post("/", async(req, res) => {
 
 // DELETE request to remove identified resource
 // DELETE api/v1/notes/4
-router.delete("/:id", (req, res) => {
-    res.send("Delete a single resource")
+router.delete("/:id", async (req, res) => {
+    // res.send("Delete a single resource")
+    const idx = req.params.id
+       //Check if the id is a valid MongoDB id
+       if(!mongoose.Types.ObjectId.isValid(idx)) {
+        return res.status(404).json({error: "No such note"})
+    }
+
+    // find the note post by id
+    const note = await Note.findOneAndDelete({_id: idx})
+
+    // if the note we are searching for does not exist
+    if(!note) {
+        return res.status(404).json({error: "No such note"})
+    }
+    res.status(200).json(note)
 })
 
 // PUT request to replace the entire identified resource with the request data
 // PUT api/v1/notes/2
-router.put("/:id", (req, res) => {
-    res.send("Replaces all the content of a single resource")
+router.put("/:id", async (req, res) => {
+    // res.send("Replaces all the content of a single resource")
+    const idx = req.params.id;
+
+    //Check if the id is a valid MongoDB id
+    if(!mongoose.Types.ObjectId.isValid(idx)) {
+           return res.status(404).json({error: "No such note"})
+      }
+  
+      const note = await Note.findOneAndUpdate({_id: idx}, {...req.body})
+      // if the note we are searching for does not exist
+      if(!note) {
+          return res.status(404).json({error: "No such note"})
+      }
+      res.status(200).json(note)
 })
 
 // PATCH request to replace a part of the identified resource with the request data.
